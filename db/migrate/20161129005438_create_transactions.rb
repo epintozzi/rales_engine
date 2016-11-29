29d221d8ba9e9f089b634f8d5de9fc6a4368075e
Vido <vidoseaver@gmail.com>
2016-11-28 20:04:49 -0700

    build rake file to populate database

--- db/migrate/20161129005438_create_transactions.rb
+++ db/migrate/20161129005438_create_transactions.rb
@@ -1,9 +1,9 @@
 class CreateTransactions < ActiveRecord::Migration[5.0]
   def change
     create_table :transactions do |t|
-      t.integer :credit_card_number
+      t.string :credit_card_number
       t.string :result
-      t.datetime :credit_card_expiration
+      t.datetime :credit_card_expiration_date
       t.references :invoice, foreign_key: true
 
       t.timestamps
