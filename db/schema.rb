dc0479e6488c702f1c8c7f206785d86693105465
Vido <vidoseaver@gmail.com>
2016-11-28 17:35:43 -0700

    add invoices and relationships for merchants and customers

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129001948) do
+ActiveRecord::Schema.define(version: 20161129003226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.text     "first_name"
+    t.text     "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "invoices", force: :cascade do |t|
+    t.string   "status"
+    t.integer  "merchant_id"
+    t.integer  "customer_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["customer_id"], name: "index_invoices_on_customer_id", using: :btree
+    t.index ["merchant_id"], name: "index_invoices_on_merchant_id", using: :btree
+  end
 
   create_table "merchants", force: :cascade do |t|
     t.string   "name"
@@ -21,4 +38,6 @@ ActiveRecord::Schema.define(version: 20161129001948) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "invoices", "customers"
+  add_foreign_key "invoices", "merchants"
 end
