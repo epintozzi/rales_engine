27feb35e1c73803866489cbbc7ec578c079e7bb0
Vido <vidoseaver@gmail.com>
2016-11-29 18:53:52 -0700

    serializers added. one needs fixin

---
+++ app/serializers/api/v1/item_serializer.rb
@@ -0,0 +1,7 @@
+class Api::V1::ItemSerializer < ActiveModel::Serializer
+  attributes :id, :name, :description, :unit_price, :merchant_id
+
+  def unit_price
+    (object.unit_price/100.to_f).to_s
+  end
+end
