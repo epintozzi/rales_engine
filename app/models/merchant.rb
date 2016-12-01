a32784d5323e548e40f6f368224c1604acc8ba4f
Vido <vidoseaver@gmail.com>
2016-12-01 13:37:39 -0700

    active record for all merchants by revenue, items sold, all revenue by date

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,6 +3,7 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
+  has_many :invoice_items, through: :invoices
 
   def favorite_customer
     favorite = customers.joins(:transactions).merge(Transaction.successful).group(:id).count.max_by do |k, v|
@@ -25,14 +26,25 @@ class Merchant < ApplicationRecord
     .sum("invoice_items.quantity * invoice_items.unit_price")
   end
 
+  def top_merchants_by_revenue(number)
+    Merchant.all.joins(invoices: [:invoice_items, :transactions])
+    .merge(Transaction.successful).group(:id)
+    .order("sum(invoice_items.unit_price * invoice_items.quantity) desc")
+    .limit(number)
+  end
 
-  # def revenue_by_date(date = nil)
-  #   iv = invoices
-  #
-  #   iv = ivoices.where(created_at: "#{date}") unless date.nil?
-  #
-  #   iv.joins(:transactions, :invoice_items)
-  #   .merge(Transaction.successful)
-  #   .sum("invoice_items.quantity * invoice_items.unit_price")
-  # end
+  def all_merchant_revenue_by_date(date)
+    Merchant.all.joins(invoices: [:invoice_items, :transactions])
+    .where(invoices: {created_at: "#{date}"})
+    .merge(Transaction.successful)
+    .sum("invoice_items.quantity * invoice_items.unit_price")
+  end
+
+  def top_merchants_by_items_sold(number)
+    Merchant.all.joins(invoices: [:invoice_items, :transactions])
+    .merge(Transaction.successful).group(:id)
+    .order("sum(invoice_items.quantity) desc")
+    .limit(number)
+  end
+
 end
