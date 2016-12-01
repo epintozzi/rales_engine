b731382b2ed180ccbbace5c435f10b1b72cc53fa
Vido <vidoseaver@gmail.com>
2016-12-01 10:09:40 -0700

    merge in revenue requests spec

--- spec/requests/api/v1/merchants/revenue_requests_spec.rb
+++ spec/requests/api/v1/merchants/revenue_requests_spec.rb
@@ -4,13 +4,14 @@ describe "merchant/:merchant_id?revenue" do
   context "get api/v1/merchants/:merchant_id/revenue" do
     it "returns the revenue for paid transactions merchant by id" do
 
-      merchant_1, merchant_2 = create_list(:merchant, 2)
+      date = "2012-03-16 11:55:05"
 
+      merchant_1, merchant_2 = create_list(:merchant, 2)
       customer_2, customer_3, customer_4 = create_list(:customer, 3)
 
       invoice_2 = create(:invoice, merchant_id: merchant_1.id, customer_id: customer_2.id)
 
-      invoice_3 = create(:invoice, merchant_id: merchant_1.id, customer_id: customer_3.id)
+      invoice_3 = create(:invoice, merchant_id: merchant_1.id, customer_id: customer_3.id, created_at: date)
 
       invoice_4 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer_4.id)
 
@@ -31,6 +32,38 @@ describe "merchant/:merchant_id?revenue" do
 
       expect(response).to be_success
       expect(revenue["revenue"]).to eq("0.18")
+    end
+  end
+  context "get api/v1/merchants/:merchant_id/revenue?date=x" do
+    it "returns the revenue for paid transactions merchant by id for a date" do
+      date = "2012-03-16 11:55:05"
+
+      merchant_1, merchant_2 = create_list(:merchant, 2)
+      customer_2, customer_3, customer_4 = create_list(:customer, 3)
+
+      invoice_2 = create(:invoice, merchant_id: merchant_1.id, customer_id: customer_2.id)
+
+      invoice_3 = create(:invoice, merchant_id: merchant_1.id, customer_id: customer_3.id, created_at: date)
+
+      invoice_4 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer_4.id)
+
+      transaction_2 = create(:transaction, invoice_id: invoice_2.id, result: "success")
+      transaction_3 = create(:transaction, invoice_id: invoice_2.id, result: "success")
+      transaction_4 = create(:transaction, invoice_id: invoice_3.id, result: "failed")
+      transaction_5 = create(:transaction, invoice_id: invoice_3.id, result: "success")
+      create_list(:transaction, 3, result: "success", invoice_id: invoice_4.id)
+
+      invoice_item_1 = create(:invoice_item, unit_price: 2, quantity: 1, invoice_id: invoice_2.id)
+      invoice_item_2 = create(:invoice_item, unit_price: 2, quantity: 2, invoice_id: invoice_2.id)
+      invoice_item_3 = create(:invoice_item, unit_price: 2, quantity: 3, invoice_id: invoice_3.id)
+      invoice_item_4 = create(:invoice_item, unit_price: 2, quantity: 2, invoice_id: invoice_4.id)
+
+      get "/api/v1/merchants/#{merchant_1.id}/revenue?date=#{date}"
+
+      revenue = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(revenue["revenue"]).to eq("0.06")
     end
   end
 end
