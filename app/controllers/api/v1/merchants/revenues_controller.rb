c21bc3959f694c99a2edbad266bb202f33a43f1c
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-11-30 22:46:32 -0700

    add namespacing to revenue controller

--- app/controllers/api/v1/merchants/revenues_controller.rb
+++ app/controllers/api/v1/merchants/revenues_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::Merchants::RevenuesController < ApplicationController
   def index
     merchant = Merchant.find(params[:merchant_id])
     if date_exists?
-      render json: merchant.revenue_by_date(date), serializer: RevenueSerializer
+      render json: merchant.revenue_by_date(date), serializer: Api::V1::Merchants::RevenueSerializer
     else
-      render json: merchant.revenue, serializer: RevenueSerializer
+      render json: merchant.revenue, serializer: Api::V1::Merchants::RevenueSerializer
     end
   end
   private
