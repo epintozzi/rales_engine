873c843b08f88c215dcb48f6745e8d436f6a1c8a
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-12-01 16:11:08 -0700

    Begin items/:id/most_items method

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,8 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
-  has_many :invoice_items
 
   def best_day
     best = invoices.joins(:transactions, :invoice_items).merge(Transaction.successful).group(:created_at).order("sum(invoice_items.quantity) desc").pluck(:created_at).first
-
   end
 end
