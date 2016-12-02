3a7b8331cd657a5fcd4cc2751c23015ed0401eee
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-12-01 22:56:58 -0700

    Add relationship tests to models

--- app/controllers/api/v1/customers/transactions_controller.rb
+++ app/controllers/api/v1/customers/transactions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::Customers::TransactionsController < ApplicationController
+  
   def index
     render json: Customer.find(params[:customer_id]).transactions
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "relationships" do
+    it "belongs to a merchant" do
+        item = create(:item)
+        expect(item).to respond_to(:merchant)
+      end
+
+    it "has many invoices through invoice_items" do
+      item = create(:item)
+      expect(item).to respond_to(:invoices)
+      expect(item).to respond_to(:invoice_items)
+    end
+
+    it "has many invoice_items" do
+        item = create(:item)
+        expect(item).to respond_to(:invoice_items)
+    end
+  end
+
 end
